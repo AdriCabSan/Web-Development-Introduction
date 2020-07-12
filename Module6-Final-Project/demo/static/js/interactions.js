7ded46f7f22692921909c1943fc592bea17cbe40
rleobardo <55817334+rleobardo@users.noreply.github.com>
2020-07-11 23:53:02 -0500

    Add files via upload

---
+++ Module6-Final-Project/demo/static/js/interactions.js
@@ -0,0 +1,26 @@
+var time = new Date().getTime();
+// $(document.body).bind("mousemove keypress", function () {
+//     time = new Date().getTime();
+// });
+
+// setInterval(function() {
+//     if (new Date().getTime() - time >= 1000) {
+//         window.location.reload(true);
+//     }
+// }, 1000);
+function refreshPage(){
+    window.location.reload();
+} 
+
+
+// var time = new Date().getTime();
+// $(document.body).bind("mousemove keypress", function () {
+//     time = new Date().getTime();
+// });
+
+// setInterval(function() {
+//     if (new Date().getTime() - time >= 10000) {
+//         window.location.reload(true);
+//     }
+// }, 10000);
+
